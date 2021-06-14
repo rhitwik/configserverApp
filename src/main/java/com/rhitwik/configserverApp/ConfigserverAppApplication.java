3f44bc4bad82a44278a3f81e9f2fbf0d38d32ddc
Rhitwik Sikdar <rhitwik.dev@gmail.com>
2021-06-14 13:54:44 +0530

    initial commit

---
+++ src/main/java/com/rhitwik/configserverApp/ConfigserverAppApplication.java
@@ -0,0 +1,15 @@
+package com.rhitwik.configserverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverAppApplication.class, args);
+	}
+
+}
